7c4cfadd29ea3d6cec70f31896df1ee7dc4ddcb4
Shashank <shashanke7y@gmail.com>
2023-06-23 13:49:47 -0400

    add input feild with autocomplete functionality

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import { StyleSheet, Text, SafeAreaView, Image, View } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_API_KEY } from "@env";
 
 const HomeScreen = () => {
   return (
@@ -10,6 +12,14 @@ const HomeScreen = () => {
         <Image
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={{ uri: "https://links.papareact.com/gzs" }}
+        />
+        <GooglePlacesAutocomplete
+          nearbyPlacesAPI="GooglePLacesSearcg"
+          debounce={400}
+          enablePoweredByContainer={false}
+          query={{ key: GOOGLE_MAPS_API_KEY, language: "en" }}
+          placeholder="Where from?"
+          styles={{ container: { flex: 0 }, textInput: { fontSize: 18 } }}
         />
         <NavOptions />
       </View>
