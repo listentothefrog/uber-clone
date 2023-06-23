ad2682d8b2f77e3703e6e8314f5cd696813e64fe
Shashank <shashanke7y@gmail.com>
2023-06-22 21:11:30 -0400

    wrap app with NavigationContainer component

--- App.js
+++ App.js
@@ -4,13 +4,18 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import HomeScreen from "./screens/HomeScreen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-        <StatusBar style="auto" />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <HomeScreen />
+          <StatusBar style="auto" />
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
