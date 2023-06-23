aa6e8ab1d4e250df6adcbdea58aeff970c8f1e0e
Shashank <shashanke7y@gmail.com>
2023-06-22 21:16:51 -0400

    created links for diffrent screens

--- App.js
+++ App.js
@@ -6,13 +6,27 @@ import HomeScreen from "./screens/HomeScreen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import MapScreen from "./screens/MapScreen";
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <HomeScreen />
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
           <StatusBar style="auto" />
         </SafeAreaProvider>
       </NavigationContainer>
