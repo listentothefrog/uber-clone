0f94339bdfa05ac40e8a15a6455a4fa39ed10711
Shashank <shashanke7y@gmail.com>
2023-06-22 20:07:04 -0400

    styled NavOptions Icons

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,6 @@
 import { Text, Image, TouchableOpacity, View, FlatList } from "react-native";
 import React from "react";
-
+import tw from "tailwind-react-native-classnames";
 const data = [
   {
     id: "123",
@@ -23,13 +23,13 @@ const NavOptions = () => {
         horizontal
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <TouchableOpacity>
+          <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
             <View>
               <Image
                 style={{ width: 120, height: 120, resizeMode: "contain" }}
                 source={{ uri: item.image }}
               />
-              <Text>{item.title}</Text>
+              <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
             </View>
           </TouchableOpacity>
         )}
