55c15c92f46754ea72ac4412ac788c430bdec320
Shashank <shashanke7y@gmail.com>
2023-06-26 14:58:10 -0400

    displays the location on the embed component based on coordinates

--- components/Map.js
+++ components/Map.js
@@ -2,20 +2,34 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import MapView from "react-native-maps";
 import { Marker } from "react-native-maps";
-import tw from "tailwind-react-native-classnames";
 
 const Map = () => {
   return (
-    <MapView
-      styles={tw`flex-1`}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      ></MapView>
+    </View>
   );
 };
 
 export default Map;
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
