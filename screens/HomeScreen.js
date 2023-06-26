5cc9bdad552b742324d9b7293a582f3ccb98a6c4
Shashank <shashanke7y@gmail.com>
2023-06-26 13:42:57 -0400

    store searched place in redux store

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,14 @@
-import { StyleSheet, Text, SafeAreaView, Image, View } from "react-native";
+import { StyleSheet, SafeAreaView, Image, View } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_API_KEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -14,12 +17,35 @@ const HomeScreen = () => {
           source={{ uri: "https://links.papareact.com/gzs" }}
         />
         <GooglePlacesAutocomplete
-          nearbyPlacesAPI="GooglePLacesSearcg"
-          debounce={400}
-          enablePoweredByContainer={false}
-          query={{ key: GOOGLE_MAPS_API_KEY, language: "en" }}
           placeholder="Where from?"
-          styles={{ container: { flex: 0 }, textInput: { fontSize: 18 } }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                loaction: details.geometry.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
+          minLength={2}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          onFail={(error) => console.error(error)}
+          query={{
+            key: GOOGLE_MAPS_API_KEY,
+            language: "en",
+          }}
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 15,
+            },
+          }}
+          enablePoweredByContainer={false}
         />
         <NavOptions />
       </View>
