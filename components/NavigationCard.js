19b518bb6c0d158cec40298017896b4b517b487e
Shashank <shashanke7y@gmail.com>
2023-06-30 11:04:21 -0400

    user can pick their destination

--- components/NavigationCard.js
+++ components/NavigationCard.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
-import React, { useEffect } from "react";
+import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "tailwind-react-native-classnames";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_API_KEY } from "@env";
-
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
 const NavigationCard = () => {
   const currentHour = new Date().getHours();
   let greetingMessage;
@@ -16,26 +18,40 @@ const NavigationCard = () => {
   } else {
     greetingMessage = "Good Evening";
   }
-
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
-      <Text style={tw`text-center text-xl`}>{greetingMessage}</Text>
-      <View style={tw`border-t border-gray-200 flex-shrink mt-3`}>
-        <GooglePlacesAutocomplete
-          placeholder="Where to?"
-          nearbyPlacesAPI="GooglePlacesSearch"
-          debounce={400}
-          minLength={2}
-          fetchDetails={true}
-          returnKeyType={"search"}
-          onFail={(error) => console.error(error)}
-          query={{
-            key: GOOGLE_MAPS_API_KEY,
-            language: "en",
-          }}
-          styles={tw`w-full bg-gray-200 rounded-md h-12 px-4`}
-          enablePoweredByContainer={false}
-        />
+      <Text style={tw`text-center text-xl font-bold`}>{greetingMessage}</Text>
+      <View
+        style={tw`border-t border-gray-100 flex-shrink relative z-20 bg-white mt-5`}
+      >
+        <View style={tw`bg-white pb-2`}>
+          <GooglePlacesAutocomplete
+            placeholder="Where to?"
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  loaction: details.geometry.location,
+                  description: data.description,
+                })
+              );
+              navigation.navigate("RideOptionsCard");
+            }}
+            minLength={2}
+            fetchDetails={true}
+            returnKeyType={"search"}
+            onFail={(error) => console.error(error)}
+            query={{
+              key: GOOGLE_MAPS_API_KEY,
+              language: "en",
+            }}
+            styles={autocompleteStyles}
+            enablePoweredByContainer={false}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -43,4 +59,33 @@ const NavigationCard = () => {
 
 export default NavigationCard;
 
-const styles = StyleSheet.create({});
+const autocompleteStyles = StyleSheet.create({
+  container: {
+    flex: 0,
+    width: "100%",
+  },
+  textInputContainer: {
+    backgroundColor: "transparent",
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    paddingHorizontal: 10,
+  },
+  textInput: {
+    height: 50,
+    borderRadius: 5,
+    fontSize: 18,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginTop: 20,
+    fontFamily: "UberMoveMedium",
+  },
+  predefinedPlacesDescription: {
+    color: "#1faadb",
+  },
+});
