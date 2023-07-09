b6ba55dec961d890b0c9ef50f4aa71b15496abc1
Shashank <shashanke7y@gmail.com>
2023-07-08 22:34:44 -0400

    users can enter in their destination

--- components/Map.js
+++ components/Map.js
@@ -1,45 +1,103 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { StyleSheet } from "react-native";
 import MapView from "react-native-maps";
 import { Marker } from "react-native-maps";
+import tw from "tailwind-react-native-classnames";
 import { useSelector } from "react-redux";
-import { selectOrigin } from "../slices/navSlice";
+import {
+  selectOrigin,
+  selectDestination,
+  setTravelTimeInformation,
+} from "../slices/navSlice";
+import MapViewDirections from "react-native-maps-directions";
+import { GOOGLE_MAPS_API_KEY } from "@env";
+import { useDispatch } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
 
-const Map = () => {
+export default function Map() {
   const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
+  const dispatch = useDispatch();
+  const mapRef = useRef(null);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+    });
+  }, [origin, destination]);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+    const getTravelTime = async () => {
+      fetch(
+        `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          dispatch(setTravelTimeInformation(data.rows[0].elements[0]));
+        });
+    };
+    getTravelTime();
+  }, [origin, destination]);
+
+  if (!origin) {
+    Alert.alert("Oops! Enter the Origin before preogressing");
+
+    navigation.goBack();
+  }
 
   return (
-    <View style={styles.container}>
-      {origin?.loaction?.lat && origin?.loaction?.lng ? (
-        <MapView
-          style={styles.map}
-          region={{
-            latitude: origin.loaction.lat,
-            longitude: origin.loaction.lng,
-            latitudeDelta: 0.005,
-            longitudeDelta: 0.005,
-          }}
-        >
-          {origin?.loaction && (
-            <Marker
-              coordinate={{
-                latitude: origin.loaction.lat,
-                longitude: origin.loaction.lng,
-              }}
-              title="Origin"
-              description={origin.description}
-              identifier="origin"
-            />
-          )}
-        </MapView>
-      ) : (
-        <Text>Loading map...</Text>
+    <MapView
+      ref={mapRef}
+      style={tw`flex-1`}
+      initialRegion={{
+        latitude: origin.loaction.lat,
+        longitude: origin.loaction.lng,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}
+    >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          lineDashPattern={[1]}
+          apikey={GOOGLE_MAPS_API_KEY}
+          strokeWidth={3}
+          strokeColor="#000000"
+        />
       )}
-    </View>
-  );
-};
 
-export default Map;
+      {origin?.loaction && (
+        <Marker
+          coordinate={{
+            latitude: origin ? origin.loaction.lat : 0,
+            longitude: origin ? origin.loaction.lng : 0,
+          }}
+          title="Origin"
+          description={origin.description}
+          identifier="origin"
+        />
+      )}
+
+      {destination && destination.loaction && (
+        <Marker
+          coordinate={{
+            latitude: destination.loaction.lat,
+            longitude: destination.loaction.lng,
+          }}
+          title="Destination"
+          description={destination.description}
+          identifier="destination"
+        />
+      )}
+    </MapView>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
